c0de345f9947a684d7c54ebfff5bb2cf49b06eda
unknown <vaso@opinov8.com>
2021-10-12 00:07:12 +0300

    added assertion for add-to-cart button presence

--- module3finalTest/test_items.py
+++ module3finalTest/test_items.py
@@ -6,8 +6,10 @@ class TestLanguages():
 
     def test_multilang(self, browser: webdriver, app_languages: dict[str, list[str]], user_language: str):
         browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-        actual_text = browser.find_element_by_class_name("btn-add-to-basket").text
+        add_to_cart_button = browser.find_element_by_class_name("btn-add-to-basket")
+        assert add_to_cart_button, "There is no add_to_cart_button on site"
+        actual_text = add_to_cart_button.text
         expected_text = app_languages[user_language][1]
         time.sleep(10)
-        assert actual_text == expected_text
+        assert actual_text == expected_text, f"Text is wrong on {app_languages[user_language][0]} version of website"
         
