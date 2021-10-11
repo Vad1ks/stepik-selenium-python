19b0f1d8fa8e8a421b93024b07e7d016102f2ef1
VadymS <vadiksosnovenko@gmail.com>
2021-10-12 00:47:05 +0300

    added test for add to cart button presence

--- module3finalTest/test_items.py
+++ module3finalTest/test_items.py
@@ -4,12 +4,15 @@ import time
 
 class TestLanguages():
 
-    def test_multilang(self, browser: webdriver, app_languages: dict[str, list[str]], user_language: str):
+    def test_button_is_present(self, browser: webdriver):
         browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
         add_to_cart_button = browser.find_element_by_class_name("btn-add-to-basket")
         assert add_to_cart_button, "There is no add_to_cart_button on site"
-        actual_text = add_to_cart_button.text
+
+    def test_text_on_add_to_cart_button(self, browser: webdriver, app_languages: dict[str, list[str]], user_language: str):
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+        actual_text = browser.find_element_by_class_name("btn-add-to-basket").text
         expected_text = app_languages[user_language][1]
-        time.sleep(10)
+        #time.sleep(10)
         assert actual_text == expected_text, f"Text is wrong on {app_languages[user_language][0]} version of website"
         
